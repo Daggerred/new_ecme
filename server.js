47e5ba335333a14dc0ce75428b6876d2d6634f79
Shailendra Sisodia <119807467+Daggerred@users.noreply.github.com>
2024-06-16 15:54:10 +0530

    Update server.js
    heelloo

--- server.js
+++ server.js
@@ -8,13 +8,16 @@ import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 import cors from "cors";
 import path from 'path';
+import { fileURLToPath } from 'url';
 
 // Configure environment variables
 dotenv.config();
 
 // Database configuration
 connectDB();
-
+//esmodule fix
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 // Create an Express application
 const app = express();
 
