c9aa01be83b7ce97fcdd6e773927dfba8ea9d137
Daggerred <dagger.github@gmail.com>
2024-06-16 15:44:36 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,44 @@
+import express from "express";
+import colors from "colors";
+import dotenv from "dotenv";
+import morgan from "morgan";
+import connectDB from "./config/db.js";
+import authRoutes from "./routes/authRoute.js";
+import categoryRoutes from "./routes/categoryRoutes.js";
+import productRoutes from "./routes/productRoutes.js";
+import cors from "cors";
+import path from 'path';
+
+// Configure environment variables
+dotenv.config();
+
+// Database configuration
+connectDB();
+
+// Create an Express application
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+app.use(morgan("dev"));
+app.use(express.static(path.join(__dirname, './client/build')))
+
+// Routes
+app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/category", categoryRoutes);
+app.use("/api/v1/product", productRoutes);
+
+// Root endpoint
+app.use('*',function (req,res) {
+  res.sendFile(path.join(__dirname,'./client/build/index.html'))
+})
+
+// Port and mode
+const PORT = process.env.PORT || 8000;
+const MODE = process.env.DEV_MODE || "development";
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server Running on ${MODE} mode on port ${PORT}`.bgCyan.white);
+});
